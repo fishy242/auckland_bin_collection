a355d45cdf65dda18648abf79a0d044159a4491e
fishy <fishy242@gmail.com>
2024-05-24 15:27:00 +1200

    remove async

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,6 @@ import pytest
 
 
 @pytest.fixture(autouse=True)
-async def auto_enable_custom_integrations(enable_custom_integrations):
+def auto_enable_custom_integrations(enable_custom_integrations):
     """Enable custom integrations defined in the test dir."""
     yield
