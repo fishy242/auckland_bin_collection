e5ced4a5003c0449aa42afcc21fbe11481ca6d91
fishy <fishy242@gmail.com>
2024-09-06 15:23:04 +1200

    Replace deprecate async_forward_entry_setup

--- custom_components/auckland_bin_collection/__init__.py
+++ custom_components/auckland_bin_collection/__init__.py
@@ -1,13 +1,15 @@
 """Auckland Bin Collection component."""
 import logging
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
+
 from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
-PLATFORM = Platform.SENSOR
+PLATFORMS = [Platform.SENSOR]
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -15,7 +17,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
     hass.data.setdefault(DOMAIN, {})
 
-    await hass.config_entries.async_forward_entry_setup(entry, PLATFORM)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     _LOGGER.debug("Auckland Bin Collection init entry setup")
 
@@ -25,7 +27,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry"""
 
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, PLATFORM)
+    unload_ok = True
+    for component in PLATFORMS:
+        if await hass.config_entries.async_forward_entry_unload(entry, component) == False:
+            unload_ok = False
 
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
